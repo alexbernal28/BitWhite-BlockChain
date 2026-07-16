696bc68f3156472b511e01898fbc9df338a1de05
Alex José Rodríguez Taveras <alexrodrigueztaveras@gmail.com>
2026-07-15 23:01:45 -0400

    Tablas: Usuario, rol, detalles empresa y ciudadano

---
+++ bitwhite-backend/src/infrastructure/models/auth/User.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../persistence/database.js';
+
+const User = sequelize.define('User', {
+  id:     { type: DataTypes.INTEGER(), primaryKey: true },
+  name:   { type: DataTypes.STRING(40), allowNull: false },
+  email:  { type: DataTypes.STRING(90), allowNull: false,  
+            unique: {
+          name: 'uq_users_email'
+    } },
+  password: { type: DataTypes.STRING(64), allowNull: false },
+  rolId:    { type: DataTypes.SMALLINT(), allowNull: false, 
+            references:{model:{ tableName: 'roles', schema: 'auth' },
+                        key: 'id'} },
+}, {
+  schema: 'auth',
+  tableName: 'users',
+  timestamps: true
+});
+
+
+<<<<<<< Updated upstream
+export default User; 
+=======
+export default User;
+>>>>>>> Stashed changes
